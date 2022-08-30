86c0fe1b53ef2428d0399610c56f37b9e6cf4e1a
sema4-romanbarabash <L!verpool1970>
2022-08-30 12:20:13 +0300

    refactor job schemas

--- backend/db/repository/jobs.py
+++ backend/db/repository/jobs.py
@@ -5,11 +5,17 @@ from backend.schemas.jobs import JobCreate
 
 
 def create_new_job(job: JobCreate, db: Session, owner_id: int):
-    job = Job(**job.dict(), owner_id=owner_id)
-    db.add(job)
+    job_model = Job(title=job.title,
+                    company=job.company,
+                    location=job.location,
+                    description=job.description,
+                    company_url=job.company_url,
+                    date_posted=job.date_posted,
+                    owner_id=owner_id)
+    db.add(job_model)
     db.commit()
-    db.refresh(job)
-    return job
+    db.refresh(job_model)
+    return job_model
 
 
 def retreive_job(id: int, db: Session):
@@ -20,4 +26,3 @@ def retreive_job(id: int, db: Session):
 def list_jobs(db: Session):
     jobs = db.query(Job).filter(Job.is_active == True).all()
     return jobs
-
--- backend/schemas/jobs.py
+++ backend/schemas/jobs.py
@@ -4,28 +4,22 @@ from typing import Optional
 from pydantic import BaseModel
 
 
-class JobBase(BaseModel):
-    title: Optional[str] = None
-    company: Optional[str] = None
-    company_url: Optional[str] = None
-    location: Optional[str] = "Remote"
-    description: Optional[str] = None
-    date_posted: Optional[date] = datetime.now().date()
-
-
-class JobCreate(JobBase):
+class JobCreate(BaseModel):
     title: str
     company: str
     location: str
     description: str
+    company_url: Optional[str] = None
+    description: Optional[str] = None
+    date_posted: Optional[date] = datetime.now().date()
 
 
-class ShowJob(JobBase):
+class ShowJob(BaseModel):
     title: str
     company: str
-    company_url: Optional[str]
     location: str
-    date_posted: date
+    company_url: Optional[str]
+    date_posted: Optional[date]
     description: Optional[str]
 
     class Config():
